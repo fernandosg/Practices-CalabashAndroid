94a9ae74728ff8250633a57ceb278a3a786ed7ef
fsgom <fsg_tmp@hotmail.com>
2016-08-14 15:05:00 -0500

    Checking the button click in Calabash and the screenshot in the app

--- features/my_first.feature
+++ features/my_first.feature
@@ -1,5 +1,6 @@
-Feature: Login feature
-
-  Scenario: As a valid user I can log into my app
-    When I press "Login"
-    Then I see "Welcome to coolest app ever"
+Feature: Login and View the List Activity
+  Scenario: Main Activity
+    Given I am on Main Activity Screen
+    Then I enter "fernando@email.com" in the "email" field
+    Then I enter "password" in the "password" field
+    Then I press button with id "login"
--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -1,1 +1,15 @@
 require 'calabash-android/calabash_steps'
+Given /^I am on Main Activity Screen$/ do
+end
+Then /^I enter "([^"]*)" in the "([^"]*)" field$/ do |text,field|
+  touch("android.widget.EditText id:'#{field}'")
+  keyboard_enter_text text
+end
+
+Then /^I press button with id "([^"]*)"$/ do |id|
+  sleep(1)
+  screenshot_embed({:name=>"main_activity"})
+  touch("android.widget.Button id:'#{id}'")
+  sleep(1)
+  screenshot_embed({:name=>"list_activity"})
+end
